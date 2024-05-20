0b35f01e02e57afbbb8e8e0b5a940feac284b4bf
felipeleal27 <felipe.leal@aluno.unifenas.br>
2024-05-20 08:46:09 -0300

    conexao_api_http

--- bitebyte/lib/app/repository/login/login_repository_impl.dart
+++ bitebyte/lib/app/repository/login/login_repository_impl.dart
@@ -2,6 +2,7 @@
 // import 'package:bitebyte/app/core/rest_client/rest_client.dart';
 import 'package:bitebyte/app/repository/login/login_repository.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class LoginRepositoryImpl extends LoginRepository {
   // final RestClient _restClient;
@@ -28,15 +29,27 @@ class LoginRepositoryImpl extends LoginRepository {
       //   return false;
       // }
 
-      return Future.delayed(const Duration(seconds: 3), () {
-        if ((userName == 'adm@adm.com' && password == 'senha forte') ||
-            (userName == 'felipe' && password == '123') ||
-            (userName == '' && password == '')) {
-          return true;
-        } else {
-          return false;
-        }
-      });
+      //HTTP:
+      var response = await http.post(
+        Uri.parse('https://localhost:7500/api/Auth/login'),
+        body: {'email': userName, 'password': password},
+      );
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+
+      // return Future.delayed(const Duration(seconds: 3), () {
+      //   if ((userName == 'adm@adm.com' && password == 'senha forte') ||
+      //       (userName == 'felipe' && password == '123') ||
+      //       (userName == '' && password == '')) {
+      //     return true;
+      //   } else {
+      //     return false;
+      //   }
+      // });
     } catch (e) {
       if (kDebugMode) {
         print('Erro ao realizar login: $e');
